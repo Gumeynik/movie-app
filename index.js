aa7df5ba06107469c21b00ad8eb947ce7580cc44
Aleksandr Kubenko <151834458+Gumeynik@users.noreply.github.com>
2024-04-10 11:13:30 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -8,32 +8,32 @@ btnNode.addEventListener('click', function () {
     const movie = getMovieFromUser();
     if (!movie) {
         return;
-    }
+    }           
     trackFilm(movie);
     renderList(movieList);
     clearInput();
 });
 
 function trackFilm(movie) {
-    movieList.push({ value: movie, index: movieList.length });
+    movieList.push({ value: movie, checked: false, index: movieList.length });
 };
-
 
 function getMovieFromUser() {
     if (inputNode.value === '' ) {
         return null;
     }
-
     return inputNode.value;
 };
+
 function clearInput() {
     inputNode.value = '';
 };
+
 function renderList(movieList) {
     let movieListHTML = '';
 
     movieList.forEach((movie, index) => {
-        movieListHTML += `<li class='item-movie'><input type="checkbox" class="checkbox"><p class='item-text'>${movie.value}</p><button class='delete-button' data-index='${index}'><span class='line-btn1'></span> <span class='line-btn2'></span></button></li>`;
+        movieListHTML += `<li class='item-movie ${movie.checked ? 'checked' : ''}' data-index='${movie.index}'><input type="checkbox" class="checkbox" ${movie.checked ? 'checked' : ''}><p class='item-text'>${movie.value}</p><button class='delete-button' data-index='${movie.index}'><span class='line-btn1'></span> <span class='line-btn2'></span></button></li>`;
     });
 
     listNode.innerHTML = `<ul class='list-movie'>${movieListHTML}</ul> `;
@@ -42,24 +42,16 @@ function renderList(movieList) {
         button.addEventListener('click', function () {
             const index = this.dataset.index;
             movieList.splice(index, 1); 
-
+            renderList(movieList); 
         });
     });
     listNode.addEventListener('click', function (event) {
         const target = event.target;
         if (target.classList.contains('checkbox')) {
             const listItem = target.closest('.item-movie');
-            const deleteButton = listItem.querySelector('.delete-button');
-    
-            if (target.checked) {
-                listItem.classList.add('checked');
-                deleteButton.classList.add('checked');
-            } else {
-                listItem.classList.remove('checked');
-                deleteButton.classList.remove('checked');
-            }
-            renderList(movieList); 
+            const index = listItem.dataset.index;
+            movieList[index].checked = target.checked;
+            renderList(movieList);
         }
     });
-
 }
